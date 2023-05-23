bb7d2dc761a71241db98ce2d131e64b2b085d954
Dasewasia10 <daniel.stonford10@gmail.com>
2023-05-23 11:29:10 +0800

    setting api login nondummy

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,12 +1,36 @@
-import data from "./dummyApi.json";
-import data2 from "./dummyApi2.json";
+import axios from "axios";
+
+async function getLoginData({ email, password }) {
+    try {
+        const response = await axios.post(
+            "http://192.168.137.1:8000/api/login",
+            { email_user: email, password }
+        );
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
 
 async function getData() {
-	return data;
+    try {
+        const response = await axios.get("192.168.137.1/api/");
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 }
 
 async function getData2() {
-	return data2;
+    try {
+        const response = await axios.get("URL_API/data2");
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 }
 
-export { getData, getData2 };
+export { getLoginData, getData, getData2 };
--- frontend/src/component/login.jsx
+++ frontend/src/component/login.jsx
@@ -1,105 +1,127 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { getLoginData } from "../api/api";
 
 const Login = ({ handleLogin }) => {
-	const handleClick = () => {
-		handleLogin();
-	};
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-	return (
-		<div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-			<div className="max-w-md w-full space-y-8">
-				<div>
-					<img
-						className="mx-auto h-12 w-auto"
-						src="../src/assets/react.svg"
-						alt="Workflow"
-					/>
-					<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-						Login
-					</h2>
-				</div>
-				<form className="mt-8 space-y-6" action="#" method="POST">
-					<input type="hidden" name="remember" defaultValue="true" />
-					<div className="rounded-md shadow-sm -space-y-px">
-						<div>
-							<label htmlFor="email-address" className="sr-only">
-								Email address
-							</label>
-							<input
-								id="email-address"
-								name="email"
-								type="email"
-								autoComplete="email"
-								required
-								className="appearance-none rounded-none relative block
-                  w-full px-3 py-2 border border-gray-300
-                  placeholder-gray-500 text-gray-900 rounded-t-md
-                  focus:outline-none focus:ring-indigo-500
-                  focus:border-indigo-500 focus:z-10 sm:text-sm"
-								placeholder="Email address"
-							/>
-						</div>
-						<div>
-							<label htmlFor="password" className="sr-only">
-								Password
-							</label>
-							<input
-								id="password"
-								name="password"
-								type="password"
-								autoComplete="current-password"
-								required
-								className="appearance-none rounded-none relative block
-                  w-full px-3 py-2 border border-gray-300
-                  placeholder-gray-500 text-gray-900 rounded-b-md
-                  focus:outline-none focus:ring-indigo-500
-                  focus:border-indigo-500 focus:z-10 sm:text-sm"
-								placeholder="Password"
-							/>
-						</div>
-					</div>
+    const handleClick = async (e) => {
+        e.preventDefault();
+        try {
+            // Mengirim permintaan login ke API menggunakan getLoginData()
+            const response = await getLoginData({ email, password });
 
-					<div className="flex items-center justify-between">
-						<div className="flex items-center">
-							<input
-								id="remember-me"
-								name="remember-me"
-								type="checkbox"
-								className="h-4 w-4 text-indigo-600 focus:ring-indigo-500
+            // Menangani respons dari API
+            // Misalnya, menyimpan token ke dalam local storage, menjalankan fungsi handleLogin, dll.
+
+            // Contoh penggunaan handleLogin:
+            handleLogin();
+            console.log(response);
+        } catch (error) {
+            console.error(error);
+            // Menangani error, misalnya menampilkan pesan error pada halaman login
+        }
+    };
+
+    return (
+        <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div className="max-w-md w-full space-y-8">
+                <div>
+                    <img
+                        className="mx-auto h-12 w-auto"
+                        src="../src/assets/react.svg"
+                        alt="Workflow"
+                    />
+                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                        Login
+                    </h2>
+                </div>
+                <form className="mt-8 space-y-6" action="#" method="POST">
+                    <input type="hidden" name="remember" defaultValue="true" />
+                    {/*Input Email*/}
+                    <div className="rounded-md shadow-sm -space-y-px">
+                        <div>
+                            <label htmlFor="email-address" className="sr-only">
+                                Email address
+                            </label>
+                            <input
+                                id="email-address"
+                                name="email"
+                                type="email"
+                                autoComplete="email"
+                                required
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="appearance-none rounded-none relative block
+            w-full px-3 py-2 border border-gray-300
+            placeholder-gray-500 text-gray-900 rounded-t-md
+            focus:outline-none focus:ring-indigo-500
+            focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                placeholder="Email address"
+                            />
+                        </div>
+                        {/* Input Password */}
+                        <div>
+                            <label htmlFor="password" className="sr-only">
+                                Password
+                            </label>
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                autoComplete="current-password"
+                                required
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="appearance-none rounded-none relative block
+            w-full px-3 py-2 border border-gray-300
+            placeholder-gray-500 text-gray-900 rounded-b-md
+            focus:outline-none focus:ring-indigo-500
+            focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                placeholder="Password"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="flex items-center justify-between">
+                        <div className="flex items-center">
+                            <input
+                                id="remember-me"
+                                name="remember-me"
+                                type="checkbox"
+                                className="h-4 w-4 text-indigo-600 focus:ring-indigo-500
                   border-gray-300 rounded"
-							/>
-							<label
-								htmlFor="remember-me"
-								className="ml-2 block text-sm text-gray-900">
-								Remember me
-							</label>
-						</div>
-					</div>
+                            />
+                            <label
+                                htmlFor="remember-me"
+                                className="ml-2 block text-sm text-gray-900"
+                            >
+                                Remember me
+                            </label>
+                        </div>
+                    </div>
 
-					<div>
-						<Link to="/">
-							<button
-								onClick={handleClick}
-								type="submit"
-								className="group relative w-full flex justify-center
-                py-2 px-4 border border-transparent text-sm font-medium
-                rounded-md text-white bg-indigo-600 hover:bg-indigo-700
-                focus:outline-none focus:ring-2 focus:ring-offset-2
-                focus:ring-indigo-500">
-								<span className="absolute left-0 inset-y-0 flex items-center pl-3">
-									<div
-										className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-										aria-hidden="true"
-									/>
-								</span>
-								Login
-							</button>
-						</Link>
-					</div>
-				</form>
-			</div>
-		</div>
-	);
+                    <div>
+                        <Link to="/">
+                            {/* Tombol Login */}
+                            <button
+                                onClick={handleClick}
+                                type="submit"
+                                className="group relative w-full flex justify-center
+            py-2 px-4 border border-transparent text-sm font-medium
+            rounded-md text-white bg-indigo-600 hover:bg-indigo-700
+            focus:outline-none focus:ring-2 focus:ring-offset-2
+            focus:ring-indigo-500"
+                            >
+                                Login
+                            </button>
+                        </Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
