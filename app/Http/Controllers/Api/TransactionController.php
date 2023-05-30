aa9901b2ef49f93746d33420692250b411c83700
NawfalHadi <nawfalhdi@gmail.com>
2023-05-30 11:37:19 +0800

    api: could get api transaction

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Models\Pemesanan;
+use App\Models\Transaksi;
+
+use Illuminate\Support\Facades\Auth;
 
 class TransactionController extends Controller
 {
@@ -34,6 +37,27 @@ class TransactionController extends Controller
     }
 
     public function getTransaction(Request $request){
+        $filter = $request->input('filter');
+        $limit = $request->input('limit', false);
 
+        $userId = Auth::id();
+
+        $query = Transaksi::query()->where('id_user', $userId);
+
+        if ($filter === 'newest') {
+            $query->orderByDesc('created_at');
+        } elseif ($filter === 'latest') {
+            $query->orderBy('created_at');
+        }
+
+        if ($limit === 'true') {
+            $query->limit(4);
+        }
+
+        $transactions = $query->get();
+
+        return response()->json([
+            'data' => $transactions,
+        ]);
     }
 }
